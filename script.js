b04dc9f174d8ff80f3a7311672642c3b936209a1
Abdullah Teladia <at47356@gmail.com>
2022-09-05 22:17:03 +0100

    Experimenting

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
  // let msg = prompt("Who's there?","" );
 // alert("Hello " + msg);
+
+function forLoop () {
+    for (i = 1; i < 10; i++ ) {
+        console.log(i);
+    }
+} 
+
+// styling
+console.log('%c complete style ','font-size: 30px; color: green; background: yellow;');
+
+console.assert(1 === 2, 'wrong all of it just wrong');
